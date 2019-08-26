f750bd76306de285f9f7d8bec61597481260722f
Ólafur Helgi Jónsson <olli.jonsson@gmail.com>
2019-08-26 14:18:06 +0000

    opna með safari

--- web/src/components/facebook-ios-message.tsx
+++ web/src/components/facebook-ios-message.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
 
 export default function FacebookIOSMessage() {
-  return <div>Hæ iphone notandi</div>;
+  return (
+    <div>
+      Hæ iphone notandi <br />
+      <a href="safari://https://aime.moon.do" target="_blank">
+        Opna með safari
+      </a>
+    </div>
+  );
 }
