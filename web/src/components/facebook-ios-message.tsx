8fbb8715b7a43e40863e61e3e73430a9fbfc1448
Ólafur Helgi Jónsson <olli.jonsson@gmail.com>
2019-08-26 17:27:50 +0000

    link test

--- web/src/components/facebook-ios-message.tsx
+++ web/src/components/facebook-ios-message.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 
+// <a href="x-web-search://?https://aime.moon.do">Opna með safari</a>
+
 export default function FacebookIOSMessage() {
   return (
     <div>
       Hæ iphone notandi <br />
-      <a href="x-web-search://?https://aime.moon.do">Opna með safari</a>
       <link rel="alternate" href="safari://aime.moon.do">
         Opna líka með safari
       </link>
