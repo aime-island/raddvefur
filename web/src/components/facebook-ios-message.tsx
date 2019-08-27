e5f32bd0c523deccdbdbaf2b199a57f3ff4fa6bd
Ólafur Helgi Jónsson <olli.jonsson@gmail.com>
2019-08-27 12:05:54 +0000

    aimemoondo

--- web/src/components/facebook-ios-message.tsx
+++ web/src/components/facebook-ios-message.tsx
@@ -5,6 +5,7 @@ export default function FacebookIOSMessage() {
     <div>
       Hæ iphone notandi <br />
       <a href="googlechrome://aime.moon.do">Opna með Chrome</a>
+      <a href="https://aime.moon.do">Aime.moon.do</a>
     </div>
   );
 }
