e98358aacb4ead0ab0bb2be76579e6771cbee6e4
Ólafur Helgi Jónsson <olli.jonsson@gmail.com>
2019-08-27 11:09:34 +0000

    googlechrome

--- web/src/components/facebook-ios-message.tsx
+++ web/src/components/facebook-ios-message.tsx
@@ -1,17 +1,10 @@
 import * as React from 'react';
 
 export default function FacebookIOSMessage() {
-  var iframe = document.createElement('iframe');
-  document.body.appendChild(iframe);
-  iframe.setAttribute('style', 'display:none;');
-  iframe.src = 'youtube://launch?var=val';
-
   return (
     <div>
       Hæ iphone notandi <br />
-      <a href="safaris://https://aime.moon.do" target="_blank">
-        Opna með safari
-      </a>
+      <a href="googlechrome://aime.moon.do">Opna með Chrome</a>
     </div>
   );
 }
