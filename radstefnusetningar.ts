2e01fb943d516b4a5cfbf44282ad4fa5b737e4b6
ViktorSveins <viktorsveins@gmail.com>
2019-10-15 10:48:53 +0000

    static list for sentances

---
+++ radstefnusetningar.ts
@@ -0,0 +1,68 @@
+const firstaRound = [
+    {
+        id: 1,
+        text: ""
+    },
+    {
+        id: 2,
+        text: ""
+    },
+    {
+        id: 3,
+        text: ""
+    },
+    {
+        id: 4,
+        text: ""
+    },
+    {
+        id: 5,
+        text: ""
+    }   
+]
+
+const secondRound = [
+    {
+        id: 1,
+        text: ""
+    },
+    {
+        id: 2,
+        text: ""
+    },
+    {
+        id: 3,
+        text: ""
+    },
+    {
+        id: 4,
+        text: ""
+    },
+    {
+        id: 5,
+        text: ""
+    }   
+]
+
+const thirdRound = [
+    {
+        id: 1,
+        text: ""
+    },
+    {
+        id: 2,
+        text: ""
+    },
+    {
+        id: 3,
+        text: ""
+    },
+    {
+        id: 4,
+        text: ""
+    },
+    {
+        id: 5,
+        text: ""
+    }   
+]
