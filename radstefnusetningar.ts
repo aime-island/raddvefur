8644e08894aee263e857c7b5446f5806bf1b5b87
ViktorSveins <viktorsveins@gmail.com>
2019-10-15 11:03:02 +0000

    david tharf tolvuna sina push

--- radstefnusetningar.ts
+++ radstefnusetningar.ts
@@ -1,68 +1,68 @@
 const firstaRound = [
     {
         id: 1,
-        text: ""
+        text: "Á íslensku má alltaf finna svar og orða stórt og smátt sem er og var"
     },
     {
         id: 2,
-        text: ""
+        text: "og hún á orð sem geyma gleði og sorg, um gamalt líf og nýtt í sveit og borg."
     },
     {
         id: 3,
-        text: ""
+        text: "Á vörum okkar verður tungan þjál, þar vex og grær og dafnar okkar mál."
     },
     {
         id: 4,
-        text: ""
+        text: "Að gæta hennar gildir hér og nú, það gerir enginn - nema ég og þú."
     },
     {
         id: 5,
-        text: ""
+        text: "Fimm"
     }   
 ]
 
 const secondRound = [
     {
         id: 1,
-        text: ""
+        text: "Hann fór fram á það að Jólasveinasetrið yrði flutt frá Norðurpólnum á Suðurheimskautið."
     },
     {
         id: 2,
-        text: ""
+        text: "Orðasambandið „að fá sér kríu“ er sótt í háttalag fuglsins."
     },
     {
         id: 3,
-        text: ""
+        text: "Best er vatn úr borholum, því næst uppsprettum og lindum."
     },
     {
         id: 4,
-        text: ""
+        text: "Í þjóðtrú hafa menn löngum verið blendnir í afstöðu sinni til kattarins."
     },
     {
         id: 5,
-        text: ""
+        text: "Óljósar og mótsagnakenndar heimildir eru um hvernig Íslendingar tóku þessari nýjung."
     }   
 ]
 
 const thirdRound = [
     {
         id: 1,
-        text: ""
+        text: "Eitt"
     },
     {
         id: 2,
-        text: ""
+        text: "Tvö"
     },
     {
         id: 3,
-        text: ""
+        text: "Þrjú"
     },
     {
         id: 4,
-        text: ""
+        text: "Fjögur"
     },
     {
         id: 5,
-        text: ""
+        text: "Fimm"
     }   
 ]
