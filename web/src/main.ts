668cb678d87eb52aaca04d9c0037c798d4243d19
ollijonsson <olli.jonsson@gmail.com>
2019-10-18 16:35:06 +0000

    fix

--- web/src/main.ts
+++ web/src/main.ts
@@ -29,11 +29,10 @@ document.addEventListener('DOMContentLoaded', async () => {
       window.location.href = 'https://samromur.page.link/redirect';
     } */
   } else {
-    renderFacebook();
-    /* if (typeof window.IntersectionObserver === 'undefined') {
+    if (typeof window.IntersectionObserver === 'undefined') {
       await require('intersection-observer');
     }
     const App = require('./components/app').default;
-    render(React.createElement(App), document.getElementById('root')); */
+    render(React.createElement(App), document.getElementById('root'));
   }
 });
