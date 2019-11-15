d860d059294f785ed6734ea047de93d9c97fb797
Ólafur Helgi Jónsson <olli.jonsson@gmail.com>
2019-11-15 23:47:18 +0000

    max lengd

--- web/src/components/pages/contribution/speak/speak.tsx
+++ web/src/components/pages/contribution/speak/speak.tsx
@@ -65,7 +65,7 @@ import {
 import './speak.css';
 
 const MIN_RECORDING_MS = 1000;
-const MAX_RECORDING_MS = 10000;
+const MAX_RECORDING_MS = 15000;
 const MIN_VOLUME = 1;
 
 const DEFAULT_LANGUAGE = 'islenska';
