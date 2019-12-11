d8447e1deff51dc0eefaf77359296df5cd3e72ac
Ólafur Helgi Jónsson <olli.jonsson@gmail.com>
2019-12-11 14:16:16 +0000

    polyfill

--- web/src/polyfill.js
+++ web/src/polyfill.js
@@ -1,3 +1,12 @@
-if (!window.MediaRecorder) {
+const isMicrophoneSupported = () => {
+  return (
+    (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) ||
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia
+  );
+};
+
+if (!isMicrophoneSupported() || !window.MediaRecorder) {
   window.MediaRecorder = require('./audio-recorder-polyfill');
 }
